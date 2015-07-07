e47f45f0ef60b6d9d095e56f4a1070f980fbba41
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-07-07 10:40:54 -0500

    Finished quiz game

--- global.js
+++ global.js
@@ -1,21 +1,28 @@
-var question1= prompt("What is the capitol of Nebraska? 1) Lincoln 2)Wilber 3)Council Bluffs 4)Omaha");
-alert("The answer is 4)Omaha");
+var question1 = {question: "What is the capitol of Nebraska?", response: "1) Lincoln 2)Wilber 3)Council Bluffs 4)Omaha" ,answer: 4};
 
-var question2= prompt("What is the name of the UNL football team? 1)Longhorns 2)Huskers 3)Sabertooth Tigers 4)Wheat");
-alert("The answer is 2)Huskers");
+var question2 = {question: "What is the name of the UNL football team?", response:" 1)Longhorns 2)Huskers 3)Sabertooth Tigers 4)Wheat", answer: 2};
 
-var question3= prompt("What was in the building before OCS? 1)InCommon 2)Donut Stop 3)Kinkos 4)Burger King");
-alert("The answer is 1)InCommon");
+var question3 = {question: "What was in the building before OCS?", response:" 1)InCommon 2)Donut Stop 3)Kinkos 4)Burger King", answer: 1};
 
-var question4= prompt("What does Donut Stop sell? 1)cat posters 2)donuts 3)puzzles 4)smurfs");
-alert("The answer is 2)donuts");
+var question4 = {question: "What does Donut Stop sell?", response: " 1)cat posters 2)donuts 3)puzzles 4)smurfs", answer: 2};
 
-var question5= prompt("What kind of food is served at Bohemian Cafe? 1)German 2)Mexican 3)Czech 4)Polish");
-alert("The answer is 3)Czech");
+var question5 = {question: "What kind of food is served at Bohemian Cafe?", response:" 1)German 2)Mexican 3)Czech 4)Polish", answer: 3};
 
-var array= [question1, question2, question3, question4, question5]
 
-for(count = 0; count > 5; count++){
-  
+var array= [question1, question2, question3, question4, question5];
+var arrayLength = array.length;
+var score = 0;
+
+for(x = 0; x < arrayLength; x++){
+  var question= array[x];
+  console.log(question.question);
+  var answer= prompt(question.response);
+  answer= parseInt(answer);
+  if (answer === question.answer){
+    score++
+  }
 }
 
+var percent= (score/5) * 100;
+
+alert("You answered " + score + " of 5 questions correctly, good for " + percent + "%!");
