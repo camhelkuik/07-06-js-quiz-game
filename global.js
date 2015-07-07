2407831d083c062b3397acee8939b7302b62405a
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-07-06 22:13:57 -0500

    Started quiz game

---
+++ global.js
@@ -0,0 +1,21 @@
+var question1= prompt("What is the capitol of Nebraska? 1) Lincoln 2)Wilber 3)Council Bluffs 4)Omaha");
+alert("The answer is 4)Omaha");
+
+var question2= prompt("What is the name of the UNL football team? 1)Longhorns 2)Huskers 3)Sabertooth Tigers 4)Wheat");
+alert("The answer is 2)Huskers");
+
+var question3= prompt("What was in the building before OCS? 1)InCommon 2)Donut Stop 3)Kinkos 4)Burger King");
+alert("The answer is 1)InCommon");
+
+var question4= prompt("What does Donut Stop sell? 1)cat posters 2)donuts 3)puzzles 4)smurfs");
+alert("The answer is 2)donuts");
+
+var question5= prompt("What kind of food is served at Bohemian Cafe? 1)German 2)Mexican 3)Czech 4)Polish");
+alert("The answer is 3)Czech");
+
+var array= [question1, question2, question3, question4, question5]
+
+for(count = 0; count > 5; count++){
+  
+}
+
